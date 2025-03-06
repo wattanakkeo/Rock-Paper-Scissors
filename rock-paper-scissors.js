3ac7e1836c25826736d707ac7e2ffc7fcf43bc75
Wattanak Keo <106860261+wattanakkeo@users.noreply.github.com>
2025-03-05 18:15:18 -0800

    - Scoreboard resets after player or computer   reaches 5. - The empty box on the left changes to an image   of rock, paper, or scissors depending on what   the user selects. - The empty box on the right changes to an image   of the computers random choice of rock, paper,   or scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -23,6 +23,8 @@ let computerWins = 0;
 
 function playRound(userChoice) {
   let computer = computerChoice();
+  const SECOND = 1000;
+  setTimeout(computerSelectionImage(computer), SECOND);
   if (userChoice === "Rock" && computer === "Paper" || userChoice === "Paper" && computer === "Scissors" || userChoice === "Scissors" && computer === "Rock") {
     computerWins+=1;
   }
@@ -34,11 +36,14 @@ function playRound(userChoice) {
   }
 
   if (userWins === 5 || computerWins === 5) {
+    userWins = 0;
+    computerWins = 0;
     alert("The game has ended");
   }
 
   userScoreTracker(userWins);
   computerScoreTracker(computerWins);
+
 }
 
 // To keep track of the score and output it on screen
@@ -52,25 +57,69 @@ function computerScoreTracker(computer) {
   computerScore.textContent = "Computer: " + computer;
 }
 
+// Created for less redundancy for an image pop up for when the user selects rock, 
+//  paper, or scissors
+
+function userSelectionImage(selection) {
+  const userImageContainer = document.querySelector("#player");
+  //Clears the previous image
+  userImageContainer.textContent = "";
+  const userImage = document.createElement("img");
+  userImage.classList.add("selectionImage")
+
+  if (selection === "Rock") {
+    userImage.src="images/rock.png";
+    userImage.style.alignItems = "center";
+  }
+  else if (selection === "Paper") {
+    userImage.src="images/paper.jpg";
+  }
+  else {
+    userImage.src="images/scissors.png";
+  }
+
+  userImageContainer.appendChild(userImage);
+}
+
+function computerSelectionImage(selection) {
+  const computerImageContainer = document.querySelector("#computer");
+  computerImageContainer.textContent = "";
+  const computerImage = document.createElement("img");
+  computerImage.classList.add("selectionImage");
+
+  if (selection === "Rock") {
+    computerImage.src = "images/rock.png";
+  }
+  else if (selection === "Paper") {
+    computerImage.src = "images/paper.jpg";
+  }
+  else {
+    computerImage.src = "images/scissors.png";
+  }
+
+  computerImageContainer.appendChild(computerImage);
+
+}
+
 // To assign Rock, Paper, or Scissors to userChoice based on which button is pressed
 let userChoice = null;
 
 function choseRock() {
   userChoice = "Rock";
   playRound(userChoice);
-  console.log(userChoice);
+  userSelectionImage(userChoice);
 }
 
 function chosePaper() {
   userChoice = "Paper";
   playRound(userChoice);
-  console.log(userChoice);
+  userSelectionImage(userChoice);
 }
 
 function choseScissors() {
   userChoice = "Scissors";
   playRound(userChoice);
-  console.log(userChoice);
+  userSelectionImage(userChoice);
 }
 
 const rock = document.querySelector("#rock");
